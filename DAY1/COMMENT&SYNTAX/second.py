1c5aa56a8130abfdbdb7e78c4fe3a196d383c2b6
Mohd Rafey <rafeymohd155@gmail.com>
2026-01-14 14:38:20 +0530

    first commit

---
+++ DAY1/COMMENT&SYNTAX/second.py
@@ -0,0 +1,11 @@
+# Write a program with two print statements and explain which one executes first.
+
+print("line one", "sinsce python is a intepreted language it will run code line by line so this code of line will execute first.");
+
+print("line two", "sinsce python is a intepreted language it will run code line by line so this code of line will execute second. ");
+
+# Write a program and comment every line of the code
+# A = 12;
+# B = 13;
+# C = A + B;
+# print(C);
