089e991b764daff79e64db2ac623d6c1fba88f3d
rafey155 <rafeymohd155@gmail.com>
2026-02-06 12:28:52 +0530

    lerning more about function and types of functions

--- PRACTICE/functions/one.py
+++ PRACTICE/functions/one.py
@@ -33,3 +33,43 @@ def sum_num(*args):
         total += num
     return total
 print(sum_num(1,2,3,4,5))
+
+# **kwargs
+def fun(**kwargs):
+    print(type(kwargs))
+    for key, value in kwargs.items():
+        print(key, "->", value)
+
+print(fun(name = "rafey", age="99", gender ="male")) 
+
+
+# returen type function
+def add_num2 (a : int,  b : int ) -> int:
+    return a+ b
+print(add_num2(5,6))
+
+# pass by vslue
+num = 10
+def modify(num):
+    num += 1
+    return num
+modify(num)
+print("original value = ",num)
+
+# pass by reference 
+my_li=[1,2,3]
+def modi_li(li):
+    my_li.append(5)
+    return li
+print("before calling function", my_li)
+
+modi_li(my_li)
+
+print("after calling function", my_li)
+
+# lambda function
+func = lambda x : x+10
+print(func(5))
+# --------------------------------------------------
+add_num3 = lambda a,b: a+b
+print(add_num3(2,4))
