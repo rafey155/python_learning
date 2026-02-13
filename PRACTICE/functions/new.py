441928703bf35a7da33c365dd5059447f635fca6
rafey155 <rafeymohd155@gmail.com>
2026-02-13 16:59:53 +0530

    learning more about function

---
+++ PRACTICE/functions/new.py
@@ -0,0 +1,7 @@
+a = int (input("Enter first value : "))
+b = int (input("Enter second value : "))
+def cal_sum(a, b):
+    sum = a + b
+    return sum
+result = cal_sum(a, b)
+print("The sum of two value is : ",result)
